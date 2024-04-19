4d0e54d5f5b1b6d03472ba2132f00f00ce8fda85
Aishwarya-Patra <patra.aishwarya1992@gmail.com>
2024-04-19 16:58:23 +0530

    commit

---
+++ JenkinsPractice/src/test/java/Jenkins/Class1.java
@@ -0,0 +1,18 @@
+package Jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class1 
+{
+	@Test
+	public void create()
+	{
+		System.out.println("---create Test---");
+	}
+	@Test
+	public void delete()
+	{
+		System.out.println("--delete--");
+	}
+
+}
---
+++ JenkinsPractice/src/test/java/Jenkins/Class2.java
@@ -0,0 +1,18 @@
+package Jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class2 
+{
+	@Test
+	public void delete()
+	{
+		System.out.println("---delete---");
+	}
+	@Test
+	public void edit()
+	{
+		System.out.println("---edit---");
+	}
+
+}
